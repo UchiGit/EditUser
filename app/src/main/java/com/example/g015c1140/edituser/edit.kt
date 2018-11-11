893132f5c19d61f080b1d0778774e7d4df150cd5
G015C1140\G015C1140 <horizon12.kuro@gmail.com>
2018-11-11 21:10:43 +0900

    1st

---
+++ app/src/main/java/com/example/g015c1140/edituser/edit.kt
@@ -0,0 +1,16 @@
+package com.example.g015c1140.edituser
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class edit : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+//        var bmp = BitmapFactory.decodeResource(resources,R.drawable.pass_visible)
+//        bmp =  Bitmap.createScaledBitmap(bmp,420,150,false)
+//        heeaderImageButton.setImageBitmap(bmp)
+    }
+}
